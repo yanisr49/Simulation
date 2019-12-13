c1c227a7c101de1fb11357bd8861700f7d023593
Richard <yanisrichard@live.fr>
2019-12-13 20:37:50 +0100

    fonctions d'evalutation indicateurs moyens

--- simulation.py
+++ simulation.py
@@ -40,6 +40,9 @@ def debut(nb_conseiller_total, nb_conseiller_appel, nb_poste_max):
     config.nb_appel_traite = 0.0
     config.nb_mail_traite = 0.0
 
+    # Genere evenement fin
+    config.echeancier = [(lambda: fin(), config.hs + 240)]
+
     # Génère mails nuit
     config.qm += lois.loi_uniform_mail_nuit() - 1
     ajout_evenement(lambda: mail.arrive_mail(), config.hs)
@@ -67,16 +70,14 @@ def fin():
 
 
 def ajout_evenement(fonction, temps):
-    i = 0
     if temps < 240:
+        i = 0
         while temps >= config.echeancier[i][1]:
             i += 1
         config.echeancier.insert(i, (fonction, temps))
 
 
 def simulation(nb_conseiller_total, nb_conseiller_appel, nb_poste_max):
-    config.hs = 0
-    config.echeancier = [(lambda: fin(), config.hs + 240)]
     debut(nb_conseiller_total, nb_conseiller_appel, nb_poste_max)
 
     while config.echeancier[0][1] < 240:
@@ -86,28 +87,61 @@ def simulation(nb_conseiller_total, nb_conseiller_appel, nb_poste_max):
         config.echeancier[0][0]()
         del config.echeancier[0]
 
-    #config.echeancier[0][0]()
+def evaluation(p0, p1, p2, p3, p4, p5):
+    global eval
+    eval += p0 * (config.nb_appel_traite / (config.nb_appel_traite + config.qt)) \
+         + p1 * (config.nb_mail_traite / (config.nb_mail_traite + config.qm)) \
+         + p2 * (1 - (config.aire_qt / (240 * (config.nb_appel_traite + config.qt)))) \
+         + p3 * (1 - (config.aire_qm / (240 * (config.nb_mail_traite + config.qm)))) \
+         + p4 * ((config.aire_ct + config.aire_cm) / (240 * config.n)) \
+         + p5 * (config.aire_ct / (240 * config.nt_max))
+
+def indicateurs():
+    global ind
+    ind[0] += config.nb_appel_traite
+    ind[1] += config.nb_mail_traite
+    ind[2] += config.qt
+    ind[3] += config.qm
+    ind[4] += config.aire_qt / (config.qt + config.ct + config.nb_appel_traite)
+    ind[5] += config.aire_qm / (config.qm + config.cm + config.nb_mail_traite)
+    ind[6] += (config.aire_ct + config.aire_cm) / (240 * config.n) * 100
+    ind[7] += config.aire_ct / (240 * config.nt_max) * 100
 
 
 if __name__ == "__main__":
     best_eval = 0
-    best_values = []
-    for nb_conseille_total in range(10, 20):
+    best_parametres = []
+    best_indicateurs = []
+
+    nb_conseille_total_min = 15
+    nb_conseille_total_max = 20
+    for nb_conseille_total in range(nb_conseille_total_min, nb_conseille_total_max):
+        print("Simulation : " + str((nb_conseille_total-nb_conseille_total_min) / float(nb_conseille_total_max-nb_conseille_total_min-1) * 100) + "%")
         for nb_poste in range(8, nb_conseille_total):
-            for nb_conseille_appel in range(nb_poste):
+            for nb_conseille_appel in range(nb_conseille_total):
                 eval = 0
-                for i in range(10):
+                ind = [0]*8
+                n = 3
+                for i in range(n):
                     simulation(nb_conseille_total, nb_conseille_appel, nb_poste)
-                    eval += (config.nb_appel_traite/(config.nb_appel_traite + config.qt)) + \
-                    (config.nb_mail_traite/(config.nb_mail_traite + config.qm)) + \
-                    (1 - (10 * (config.aire_qt/(240 * (config.nb_appel_traite + config.qt))))) + \
-                    ((config.aire_ct + config.aire_cm) / (240 * config.n)) + \
-                    (config.aire_ct / (240 * config.nt_max))
+                    evaluation(1, 1, 1, 1, 1, 1)
+                    indicateurs()
                 if eval > best_eval:
                     best_eval = eval
-                    best_values = [nb_conseille_total, nb_conseille_appel, nb_poste]
-    print("Nombre conseillé total : " + str(best_values[0]))
-    print("Nombre conseillé appel : " + str(best_values[1]))
-    print("Nombre poste : " + str(best_values[2]))
-    simulation(best_values[0], best_values[1], best_values[2])
-    fin()
+                    best_parametres = [nb_conseille_total, nb_conseille_appel, nb_poste]
+                    best_indicateurs = [(value / n) for value in ind]
+
+    print("\nNombre conseillé total : " + str(best_parametres[0]))
+    print("Nombre conseillé appel : " + str(best_parametres[1]))
+    print("Nombre poste : " + str(best_parametres[2]))
+
+    print("\neval : " + str(best_eval))
+
+    print("\nNombre appel traite : " + str(best_indicateurs[0]))
+    print("Nombre mail traite : " + str(best_indicateurs[1]))
+    print("Nombre appel non traite : " + str(best_indicateurs[2]))
+    print("Nombre mail non traite : " + str(best_indicateurs[3]))
+    print("Temps moyen dans la queue (appel) : " + str(best_indicateurs[4]) + " min")
+    print("Temps moyen dans la queue (mail) : " + str(best_indicateurs[5]) + " min")
+    print("Taux occupation conseille : " + str(best_indicateurs[6]) + " %")
+    print("Taux occupation poste telephonique : " + str(best_indicateurs[7]) + " %")
